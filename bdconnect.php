e7438260462843a5830d60377583c8a310a2599a
Lindsey <lindseynewman@hotmail.co.uk>
2016-11-03 13:47:17 +0000

    First commit

---
+++ bdconnect.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = mysql_connect("localhost","lenny88","");
+mysql_select_db("c9");
+session_start();
+
+if (false===$conn){
+    die("Connection failed");
+}
+
+else {
+    echo "Database Connected";
+}
+?>
